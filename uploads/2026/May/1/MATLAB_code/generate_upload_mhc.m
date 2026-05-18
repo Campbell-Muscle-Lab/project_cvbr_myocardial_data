c2c3ea41326e017f6788ac41cd8a04ad7ea820c2
Ken Campbell <8246018+kenatcampbellmusclelab@users.noreply.github.com>
2026-05-18 13:36:52 -0400

    Uploads for May 2026

---
+++ uploads/2026/May/1/MATLAB_code/generate_upload_mhc.m
@@ -0,0 +1,55 @@
+function generate_upload_mhc
+
+% Fix path
+addpath('C:\ken\GitHub\CampbellMuscleLab\MATLAB\MATLAB_cardiac_biobank');
+
+% Variables
+expt_data_file = '../expt_data/mhc_collated_May26.xlsx';
+output_file = '../upload/mhc_upload.csv';
+
+% Code
+
+% Start by reading the experimental data
+d = readtable(expt_data_file);
+dn = d.Properties.VariableNames'
+size_d = size(d)
+
+% Create the upload file
+up.record_id = d.hashcode;
+up.redcap_repeat_instrument = repmat({'gel_proportion'}, [size(d,1), 1]);
+up.redcap_repeat_instance = repmat({'new'}, [size(d,1), 1]);
+up.gel_prop_data_url = d.lab_archives_link;
+up.gel_prop_expt_date = datetime(d.Experiment_Date);
+up.gel_prop_mhc_alpha = d.rel_alpha_myosin;
+
+% Set the spec_number
+for i = 1 : size(d,1)
+    sp = d.Specimen_No(i);
+    if (isnumeric(sp))
+        up.gel_prop_spec_number{i} = sprintf('%.1f', sp);
+    else
+        error('ken')
+    end
+end
+
+% Set the region codes
+for i = 1 : size(d,1)
+    rc = return_region_index(d.Region{i});
+    if (~isnan(rc))
+        up.gel_prop_region{i} = sprintf('%i', rc);
+    else
+        up.gel_prop_region{i} = '';
+    end
+end
+
+
+up = columnize_structure(up);
+up = struct2table(up)
+
+up.gel_prop_expt_date.Format = 'yyyy-MM-dd';
+
+% Write out
+try
+    delete(output_file);
+end
+writetable(up, output_file, delim=',')
